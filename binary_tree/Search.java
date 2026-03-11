a9b927b3a3d97b81e854b9d31be0f87ab698b0a9
João Gabriel <jgmr0308@gmail.com>
2026-03-11 18:26:35 -0300

    Algoritmos de busca criados

--- binary_tree/Search.java
+++ binary_tree/Search.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
 public class Search {
-    public void NLR(ArrayList<Integer> list, BinaryTree tree){
-        
+    public void NLR(Node node, ArrayList<Integer> list){
+        if(node == null){
+            return;
+        }
+
+        list.add(node.value);   // N
+        NLR(node.left, list);   // L
+        NLR(node.right, list);  // R
+    }
+    public void LNR(Node node, ArrayList<Integer> list){
+        if (node == null){
+            return;
+        }
+
+        LNR(node.left, list);
+        list.add(node.value);
+        LNR(node.right, list);
+    }
+    public void LRN(Node node, ArrayList<Integer> list){
+        if (node == null){
+            return;
+        }
+
+        LRN(node.left, list);
+        LRN(node.right, list);
+        list.add(node.value);
     }
 }
